1ac50f88d78e3940804dbb9f7bfa41cf03f47612
Andrea Storace <andrea.storace.uni@gmail.com>
2022-11-29 20:52:02 +0100

    refactored styles

--- src/components/TIcon.vue
+++ src/components/TIcon.vue
@@ -14,79 +14,79 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
- .type-icon {
-   @apply border-4 w-4 h-4 rounded-full bg-contain inline-block p-2;
- }
+.type-icon {
+  @apply border-4 w-4 h-4 rounded-full bg-contain inline-block p-2;
+}
 
- .normal {
-   @apply border-normal bg-normal bg-icon-normal;
- }
+.normal {
+  @apply border-normal bg-normal bg-icon-normal;
+}
 
- .fire {
-   @apply border-fire bg-fire bg-icon-fire;
- }
+.fire {
+  @apply border-fire bg-fire bg-icon-fire;
+}
 
- .water {
-   @apply border-water bg-water bg-icon-water;
- }
+.water {
+  @apply border-water bg-water bg-icon-water;
+}
 
- .electric {
-   @apply border-electric bg-electric bg-icon-electric;
- }
+.electric {
+  @apply border-electric bg-electric bg-icon-electric;
+}
 
- .grass {
-   @apply border-grass bg-grass bg-icon-grass;
- }
+.grass {
+  @apply border-grass bg-grass bg-icon-grass;
+}
 
- .ice {
-   @apply border-ice bg-ice bg-icon-ice;
- }
+.ice {
+  @apply border-ice bg-ice bg-icon-ice;
+}
 
- .fighting {
-   @apply border-fighting border-fighting bg-fighting bg-icon-fighting;
- }
+.fighting {
+  @apply border-fighting border-fighting bg-fighting bg-icon-fighting;
+}
 
- .poison {
-   @apply border-poison bg-poison bg-icon-poison;
- }
+.poison {
+  @apply border-poison bg-poison bg-icon-poison;
+}
 
- .ground {
-   @apply border-ground bg-ground bg-icon-ground;
- }
+.ground {
+  @apply border-ground bg-ground bg-icon-ground;
+}
 
- .flying {
-   @apply border-flying bg-flying bg-icon-flying;
- }
+.flying {
+  @apply border-flying bg-flying bg-icon-flying;
+}
 
- .psychic {
-   @apply border-psychic bg-psychic bg-icon-psychic;
- }
+.psychic {
+  @apply border-psychic bg-psychic bg-icon-psychic;
+}
 
- .bug {
-   @apply border-bug bg-bug bg-icon-bug;
- }
+.bug {
+  @apply border-bug bg-bug bg-icon-bug;
+}
 
- .rock {
-   @apply border-rock bg-rock bg-icon-rock;
- }
+.rock {
+  @apply border-rock bg-rock bg-icon-rock;
+}
 
- .ghost {
-   @apply border-ghost bg-ghost bg-icon-ghost;
- }
+.ghost {
+  @apply border-ghost bg-ghost bg-icon-ghost;
+}
 
- .dragon {
-   @apply border-dragon bg-dragon bg-icon-dragon;
- }
+.dragon {
+  @apply border-dragon bg-dragon bg-icon-dragon;
+}
 
- .dark {
-   @apply border-dark bg-dark bg-icon-dark;
- }
+.dark {
+  @apply border-dark bg-dark bg-icon-dark;
+}
 
- .steel {
-   @apply border-steel bg-steel bg-icon-steel;
- }
+.steel {
+  @apply border-steel bg-steel bg-icon-steel;
+}
 
- .fairy {
-   @apply border-fairy bg-fairy bg-icon-fairy;
- }
+.fairy {
+  @apply border-fairy bg-fairy bg-icon-fairy;
+}
 </style>
